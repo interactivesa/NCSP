640758858d93781b38dfba3db84758a740e3af17
Mahmoud <oudy_mosa@hotmail.com>
2024-08-07 13:15:09 +0300

    fix en

--- assets/js/custom-en.js
+++ assets/js/custom-en.js
@@ -352,12 +352,12 @@
       creativeEffect: {
           prev: {
               // will set `translateZ(-400px)` on previous slides
-              translate: ['100%', 0, 0],
+              translate: ['-100%', 0, 0],
               opacity: 0.5
           },
           next: {
               // will set `translateX(100%)` on next slides
-              translate: ['-100%', 0, 0],
+              translate: ['100%', 0, 0],
               opacity: 0.5
           },
       },
